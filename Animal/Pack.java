c1f41912dbcae4500d0474d0bc502069e9ae3dfa
Valeriy Simbirev <valerkin2001@yandex.ru>
2024-02-08 10:47:28 +0300

    Save_8

---
+++ Animal/Pack.java
@@ -0,0 +1,21 @@
+package Animal;
+
+public class Pack extends Animal{
+    public Pack(String name, String skills, String color, String breed, String age, String birthdate){
+        super(name, skills, color, breed, age, birthdate);
+    }
+    @Override
+    public void displayCommands() {
+
+    }
+
+    @Override
+    public void teachNewCommand(String command) {
+
+    }
+
+    @Override
+    public void toChangeBreed(String changeBreed) {
+    }
+
+}
