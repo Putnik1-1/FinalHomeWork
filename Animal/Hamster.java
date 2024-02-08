6e4946e137dede9e0f88b73514e5567691ce7e8c
Valeriy Simbirev <valerkin2001@yandex.ru>
2024-02-08 10:47:02 +0300

    Save_7

---
+++ Animal/Hamster.java
@@ -0,0 +1,25 @@
+package Animal;
+
+public class Hamster extends Pet{
+    public Hamster(String name, String skills, String color, String breed, String age, String birthdate) {
+        super(name, skills, color, breed, age, birthdate);
+    }
+    @Override
+    public void displayCommands() {
+        System.out.println("Список команд для хомяка:");
+        System.out.println(getSkills());
+    }
+
+    @Override
+    public void teachNewCommand(String command) {
+        String updatedSkills = getSkills() + ", " + command;
+        setSkills(updatedSkills);
+        System.out.println("Хомяк " + getName() + " научился новой команде: " + command);
+    }
+    @Override
+    public  void toChangeBreed(String changeBreed){
+        String toChangeBreed = getBreed() + ", " + changeBreed;
+        setBreed(toChangeBreed);
+        System.out.println("Хомяку " + getName() + " изменили породу: " + changeBreed);
+    }
+}
